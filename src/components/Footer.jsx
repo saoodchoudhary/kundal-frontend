5c665bdaf1fb82676a93b89b652b366e9eaf7c28
Docs in Hindi <docsinhindi@gmail.com>
2023-12-10 22:46:10 +0530

    in

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
 
@@ -8,9 +9,11 @@ const Footer = () => {
       className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-800 dark:text-neutral-200 lg:text-left">
      
       {/* <!--Copyright section--> */}
-      <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-900">
-        <span>Copyright ©2023 All rights reserved</span>
-      
+       <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-900">
+        <span>Copyright ©2023 All rights reserved</span>-
+     <Link to="/admin">
+      Admin
+     </Link>
       </div>
     </footer>
     </div>
