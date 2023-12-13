a3f6a1e3291806e0f98ed128af33f9e3539e2621
Docs in Hindi <docsinhindi@gmail.com>
2023-12-13 19:35:51 +0530

    init

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -31,22 +31,25 @@ const CategoryPage = () => {
   return (
     <div className="bg-white max-w-[1200px] mr-auto ml-auto my-8">
       <div>
-      <div className="flex flex-wrap justify-center gap-2 sm:gap-5 md:gap-9">
-  {category.map((item, ind) => (
-    <Link key={ind} to={`/product/category/${item.name}`} className="p-1 sm:p-2">
-      <div className="rounded shadow-lg overflow-hidden">
-        <img
-          className="w-[190px] sm:w-[250px] md:w-[250px] lg:w-[300px] xl:w-[350px] h-auto sm:h-auto md:h-auto lg:h-auto xl:h-auto object-cover"
-          src={`${process.env.REACT_APP_API_URL}/Images/${item?.image}`}
-          alt="Sunset in the mountains"
-        />
-        <div className="py-4">
-          <div className="font-semibold text-center text-lg mb-2">{item.name}</div>
+      <div className="flex justify-center">
+  <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 sm:gap-5 md:gap-9">
+    {category.map((item, ind) => (
+      <Link key={ind} to={`/product/category/${item.name}`} className="p-1 sm:p-2">
+        <div className="rounded shadow-lg overflow-hidden">
+          <img
+            className="w-[190px] sm:w-[250px] md:w-[250px] lg:w-[300px] xl:w-[350px] h-auto sm:h-auto md:h-auto lg:h-auto xl:h-auto object-cover"
+            src={`${process.env.REACT_APP_API_URL}/Images/${item?.image}`}
+            alt="Sunset in the mountains"
+          />
+          <div className="py-4">
+            <div className="font-semibold text-center text-lg mb-2">{item.name}</div>
+          </div>
         </div>
-      </div>
-    </Link>
-  ))}
+      </Link>
+    ))}
+  </div>
 </div>
+
 
       
       </div>
