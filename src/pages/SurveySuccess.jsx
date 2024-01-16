22de3234299214ff68b5a6a2132b56b6448774db
Docs in Hindi <docsinhindi@gmail.com>
2024-01-16 15:04:09 +0530

    updarefds

--- src/pages/SurveySuccess.jsx
+++ src/pages/SurveySuccess.jsx
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, {  } from 'react';
 
 const SurveySuccess = () => {
   // This state variable will help manage whether to display the success message or not
 
-  const data = useSelector(state => state.survey)
-  console.log("survey",data)
-  const [submitted, setSubmitted] = useState(true);
 
   return (
     <div className="flex justify-center items-center h-screen">
-      {submitted ? (
+      
         <div className="text-center">
           <h2 className="text-3xl font-bold mb-4">Thank you for your survey!</h2>
           <p className="text-gray-600">We appreciate your valuable feedback.</p>
         </div>
-      ) : (
-        <p className="text-gray-600">Loading...</p>
-      )}
+      
     </div>
   );
 };
