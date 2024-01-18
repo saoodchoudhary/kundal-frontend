e4c8a1e12a50ac8884817f770945aacebb05d565
Docs in Hindi <docsinhindi@gmail.com>
2024-01-18 12:34:48 +0530

    dsf

--- src/pages/admin/AdminDetailsSurveyPage.jsx
+++ src/pages/admin/AdminDetailsSurveyPage.jsx
@@ -1,6 +1,7 @@
 // AdminDetailsSurveyPage.js
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Loading from '../../components/Loading';
 
 const AdminDetailsSurveyPage = () => {
   const { id } = useParams();
@@ -20,18 +21,18 @@ const AdminDetailsSurveyPage = () => {
   }, [id, isLoading]);
 
   if (isLoading) {
-    return <div className="text-center mt-8">Loading...</div>;
+    return <Loading/>;
   }
 
   return (
     <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-4">Survey Details for {survey.name}</h1>
+      <h1 className="text-3xl font-bold mb-4">Survey Details for {(survey.name === "") ? "Unknown": survey.name}</h1>
 
       {/* Basic Information Section */}
       <div className="border-t border-b border-gray-300 my-4 py-2">
         <h2 className="text-lg font-bold mb-2">Basic Information:</h2>
         <ul>
-          <li><strong>Name:</strong> {survey.name}</li>
+          <li><strong>Name:</strong> {(survey.name === "") ? "Unknown": survey.name}</li>
           <li><strong>Age:</strong> {survey.age}</li>
           <li><strong>Hair Kind:</strong> {survey.hairKind}</li>
           <li><strong>Max Products:</strong> {survey.maxProduct}</li>
@@ -40,74 +41,77 @@ const AdminDetailsSurveyPage = () => {
 
       {/* Product Information Section */}
       <div className="border-t border-b border-gray-300 my-4 py-2">
-        <h2 className="text-lg font-bold mb-2">Product Information:</h2>
+        <h2 className="text-lg font-bold mb-2">Product 1 Information:</h2>
         <ul>
-          <li><strong>Product 1:</strong> {survey.productName1}</li>
-          <li><strong>Product 2:</strong> {survey.productName2}</li>
-          <li><strong>Product 3:</strong> {survey.productName3}</li>
+          <li><strong>Product Name:</strong> {survey.productName1}</li>
+          <li><strong>Opinion :</strong> {survey.opinion1}</li>
+          <li><strong>Pros:</strong> {survey.pros1}</li>
+          <li><strong>cons:</strong> {survey.cons1}</li>
+          <li><strong>Previous Product:</strong> {survey.previousProduct_c1}</li>
+          <li><strong>Purchase Information:</strong> {survey.purchaseInformation1}</li>
+          <li><strong>Cost :</strong> {survey.cost1}</li>
+          <li><strong>Comparison :</strong> {survey.comparisonSurvey1}</li>
+          <li><strong>Ingredient Awarness :</strong> {survey.ingredientAwarness1}</li>
+          <li><strong>Willingness to Pay :</strong> {survey.willingnesstoPay1}</li>
+          <li><strong>howManyTimes:</strong> {survey.howManyTimes1}</li>
+          <li><strong>howManyTimes:</strong> {survey.usageInformation1}</li>
         </ul>
       </div>
 
-      {/* Opinions Section */}
-      <div className="border-t border-b border-gray-300 my-4 py-2">
-        <h2 className="text-lg font-bold mb-2">Opinions:</h2>
+    {survey.maxProduct > 1 && <div className="border-t border-b border-gray-300 my-4 py-2">
+        <h2 className="text-lg font-bold mb-2">Product 2 Information:</h2>
         <ul>
-          <li><strong>Opinion 1:</strong> {survey.opinion1}</li>
-          <li><strong>Opinion 3:</strong> {survey.opinion3}</li>
+          <li><strong>Product Name:</strong> {survey.productName2}</li>
+          <li><strong>Opinion :</strong> {survey.opinion2}</li>
+          <li><strong>Pros:</strong> {survey.pros2}</li>
+          <li><strong>cons:</strong> {survey.cons2}</li>
+          <li><strong>Previous Product:</strong> {survey.previousProduct_c2}</li>
+          <li><strong>Purchase Information:</strong> {survey.purchaseInformation2}</li>
+          <li><strong>Cost :</strong> {survey.cost2}</li>
+          <li><strong>Comparison :</strong> {survey.comparisonSurvey2}</li>
+          <li><strong>Ingredient Awarness :</strong> {survey.ingredientAwarness2}</li>
+          <li><strong>Willingness to Pay :</strong> {survey.willingnesstoPay2}</li>
+          <li><strong>howManyTimes:</strong> {survey.howManyTimes2}</li>
         </ul>
       </div>
-
-      {/* Pros Section */}
-      <div className="border-t border-b border-gray-300 my-4 py-2">
-        <h2 className="text-lg font-bold mb-2">Pros:</h2>
+      } 
+    {survey.maxProduct > 2 && <div className="border-t border-b border-gray-300 my-4 py-2">
+        <h2 className="text-lg font-bold mb-2">Product 3 Information:</h2>
         <ul>
-          <li>{survey.pros1}</li>
-          <li>{survey.pros2}</li>
-          <li>{survey.pros3}</li>
+          <li><strong>Product Name:</strong> {survey.productName3}</li>
+          <li><strong>Opinion :</strong> {survey.opinion3}</li>
+          <li><strong>Pros:</strong> {survey.pros3}</li>
+          <li><strong>cons:</strong> {survey.cons3}</li>
+          <li><strong>Previous Product:</strong> {survey.previousProduct_c3}</li>
+          <li><strong>Purchase Information:</strong> {survey.purchaseInformation3}</li>
+          <li><strong>Cost :</strong> {survey.cost3}</li>
+          <li><strong>Comparison :</strong> {survey.comparisonSurvey3}</li>
+          <li><strong>Ingredient Awarness :</strong> {survey.ingredientAwarness3}</li>
+          <li><strong>Willingness to Pay :</strong> {survey.willingnesstoPay3}</li>
+          <li><strong>howManyTimes:</strong> {survey.howManyTimes3}</li>
         </ul>
       </div>
+      } 
 
-      {/* Cons Section */}
-      <div className="border-t border-b border-gray-300 my-4 py-2">
-        <h2 className="text-lg font-bold mb-2">Cons:</h2>
+   
+   <div className="border-t border-b border-gray-300 my-4 py-2">
+        <h2 className="text-lg font-bold mb-2">Personal Care Routine :</h2>
         <ul>
-          <li>{survey.cons1}</li>
-          <li>{survey.cons2}</li>
-          <li>{survey.cons3}</li>
+          <li><strong>What is your personal care routine?:</strong> {survey.personalCareRoutineA}</li>
+          <li><strong>From where do you buy your personal care products? :</strong> {survey.personalCareRoutineB}</li>
+          <li><strong>What is your hair care routine?:</strong> {survey.personalCareRoutineC}</li>
         </ul>
       </div>
-
-      {/* Previous Products Section */}
-      <div className="border-t border-b border-gray-300 my-4 py-2">
-        <h2 className="text-lg font-bold mb-2">Previous Products:</h2>
+   
+   <div className="border-t border-b border-gray-300 my-4 py-2">
+        <h2 className="text-lg font-bold mb-2">Additional Information :</h2>
         <ul>
-          <li>{survey.previousProduct_c1}</li>
-          <li>{survey.previousProduct_c2}</li>
-          <li>{survey.previousProduct_c3}</li>
+          <li><strong>Allergies:</strong> {survey.allergies}</li>
+          <li><strong>Extra Feedback :</strong> {survey.extraFeedback}</li>
         </ul>
       </div>
-
-      {/* Purchase Information Section */}
-      <div className="border-t border-b border-gray-300 my-4 py-2">
-        <h2 className="text-lg font-bold mb-2">Purchase Information:</h2>
-        <ul>
-          <li>{survey.purchaseInformation1}</li>
-          <li>{survey.purchaseInformation2}</li>
-          <li>{survey.purchaseInformation3}</li>
-        </ul>
-      </div>
-
-      {/* Cost Section */}
-      <div className="border-t border-b border-gray-300 my-4 py-2">
-        <h2 className="text-lg font-bold mb-2">Cost:</h2>
-        <ul>
-          <li>{survey.cost1}</li>
-          <li>{survey.cost2}</li>
-          <li>{survey.cost3}</li>
-        </ul>
-      </div>
-
-      {/* Add similar sections for other data points */}
+      
+       
 
     </div>
   );
--- src/pages/admin/AdminSurveyPage.jsx
+++ src/pages/admin/AdminSurveyPage.jsx
@@ -7,12 +7,28 @@ const AdminSurveyPage = () => {
 const  [items, setItems] = useState()
 const [isLoading, setIsLoading]= useState(true);
 
-
+const handleDeleteSurvey = (id) =>{
+  setIsLoading(true)
+  fetch(`${process.env.REACT_APP_API_URL}/survey/deleteSurvey/${id}`,{
+    method:"DELETE"
+  })
+  .then(response => response.json())
+  .then(data => {
+    setIsLoading(false);
+  })
+  .catch(error => {
+    console.error('Error fetching survey details:', error)
+  
+    setIsLoading(false)
+  });
+  
+}
   useEffect(()=>{
     const fetchData = async()=>{
       const res = await fetch(`${process.env.REACT_APP_API_URL}/survey/getAllSurvey`);
 
       const data = await res.json();
+      console.log(data)
       setItems(data)
         setIsLoading(false)  
 
@@ -34,15 +50,19 @@ const [isLoading, setIsLoading]= useState(true);
               <th className="border border-gray-200 px-4 py-2">User Name</th>
               <th className="border border-gray-200 px-4 py-2">Product Name</th>
               <th className="border border-gray-200 px-4 py-2">View</th>
+              <th className="border border-gray-200 px-4 py-2">Delete</th>
             </tr>
           </thead>
           <tbody>
             {items.map(val => (
               <tr key={val._id}>
-               <td className="border text-center border-gray-200 px-4 py-2">{val.name}</td>
-                <td className="border border-gray-200 align-middle text-center px-4 py-2 ">{val.productName}</td>
+               <td className="border text-center border-gray-200 px-4 py-2">{(val.name === "") ? "Unknown": val.name}</td>
+                <td className="border border-gray-200 align-middle text-center px-4 py-2 ">{val.productName1}</td>
                 <td className="border text-center border-gray-200 align-middle px-4 py-2">
                   <div className='cursor-pointer inline-flex text-white py-1 px-4 rounded-md bg-cyan-500 hover:bg-cyan-600  gap-1'><Link to={`/admin/survey/details/${val._id}`} className='flex gap-1 align-middle'><FaEye className='w-3 h-3 self-center'/>View</Link></div>
+                </td>
+                <td className="border text-center border-gray-200 align-middle px-4 py-2">
+                  <div onClick={()=>handleDeleteSurvey(val._id)} className='cursor-pointer inline-flex text-white py-1 px-4 rounded-md bg-red-500 hover:bg-red-600  gap-1'>Delete</div>
                 </td>
               </tr>
             ))}
